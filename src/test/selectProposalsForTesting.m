bcb0b2585692cf063277170c09b44ae12f3d576f
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2014-11-23 16:58:30 -0500

    compute selProposals.mat

---
+++ src/test/selectProposalsForTesting.m
@@ -0,0 +1,37 @@
+function selectProposalsForTesting(resDir, vocDevKit)
+if ~isdeployed
+    addpath('../utils');
+end
+mcgDir = fullfile(resDir, 'mcg_proposals');
+outputFile = fullfile(resDir, 'selProposals.mat');
+files = getAllFiles(fullfile(vocDevKit, 'SegmentationClass'));
+masks = {};
+imgs = {};
+bboxes = zeros(0, 4);
+
+cnt = 0;
+if exist(outputFile, 'file')
+    load(outputFile, 'masks', 'imgs', 'bboxes');
+else
+    for file = files(:)'
+        cnt = cnt + 1;
+        file = file{:};
+        [~, fname, ~] = fileparts(file);
+        try
+            load(fullfile(mcgDir, [fname, '.mat']), 'candidates_mcg');
+        catch e
+            fprintf(2. '%s\n', getReport(e));
+            continue;
+        end
+        for i = 1 : 20 % take 20 from each image
+            masks{end + 1} = ...
+                ismember(candidates_mcg.superpixels, candidates_mcg.labels{i});
+            imgs{end + 1} = fname;
+            bboxes(end + 1, :) = candidates_mcg.bboxes(i, :);
+        end
+        fprintf('Read for %s (%d / %d)\n', file, cnt, numel(files));
+    end
+    fprintf('Read all\n');
+    save(outputFile, 'masks', 'imgs', 'bboxes');
+end
+
